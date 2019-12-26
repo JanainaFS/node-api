418017fa18f92d7eef16b0e3dc1920679cbedddf
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2019-12-26 16:22:18 -0300

    Controller de produto

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -4,6 +4,8 @@ const Product = mongoose.model('Product');
 
 module.exports = {
     async index(req, res) {
-        const products = await Product;
+        const products = await Product.find();
+
+        return res.json(products);
     }
 }
