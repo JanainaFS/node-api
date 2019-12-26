a3edaf470170294476f53d68189e74e7feb8402e
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2019-12-26 17:21:18 -0300

    Crud de produtos

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -9,8 +9,28 @@ module.exports = {
         return res.json(products);
     },
 
-    async store(req, res) {
-        //Criação
+    async show(req, res) {
+        const product = await Product.findById(req.params.id);
 
+        return res.json(product);
+
+    },
+
+    async store(req, res) {
+        const product = await Product.create(req.body);
+
+        return res.json(product);
+    },
+
+    async update(req, res) {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true});
+
+        return res.json(product);
+    },
+
+    async delete(req, res) {
+        const product = await Product.findByIdAndRemove(req.params.id);
+
+        return res.send();
     }
 }
