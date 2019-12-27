fe9d8674d6ce40ada20905f73da6913f91c2b1df
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2019-12-27 14:46:03 -0300

    Adicionando cors a aplicação

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 //Inicializando o app.
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //Inicializando o BD.
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useUnifiedTopology: true, useNewUrlParser: true });
